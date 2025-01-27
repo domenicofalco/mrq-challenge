102273c6a6c6d36a6260cc7bb1334ecb233319aa
memmof <59845319+domenicofalco@users.noreply.github.com>
2025-01-27 16:10:02 +0100

    improve initial loading time with suspense

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,16 +1,20 @@
-import SymbolsView from '@/components/SymbolsView';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import StatementsView from "@/components/StatementsView";
-import ProfileView from "@/components/ProfileView";
+import React, { Suspense, lazy } from 'react';
+
+const SymbolsView = lazy(() => import('@/components/SymbolsView'));
+const ProfileView = lazy(() => import('@/components/ProfileView'));
+const StatementsView = lazy(() => import('@/components/StatementsView'));
 
 const Router = () => {
   return (
+    <Suspense fallback={<div>...loading placeholder...</div>}>
       <Routes>
         <Route index element={<SymbolsView />} />
         <Route index path="profile" element={<ProfileView />} />
         <Route index path="statements" element={<StatementsView />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+    </Suspense>
   );
 };
 
